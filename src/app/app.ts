7be99882f98b4223cac01eb947431a63ca92d6a7
M1raman <276498452+Miraman-ux@users.noreply.github.com>
2026-06-23 13:55:05 +0300

    feat: прошел с 13 по 20 главы

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,42 @@
-import { Component, signal } from '@angular/core';
+import { asNativeElements, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [],
-  templateUrl: './app.html',
-  styleUrls: ['./app.css', './text.css', './border.css'],
+    selector: 'app-root',
+    imports: [],
+    templateUrl: './app.html',
+    styleUrls: ['./app.css', './text.css', './border.css'],
 })
 export class App {
-  protected readonly title = signal('el-pixel-angular');
+    public name: string = 'john';
+    public surn: string = 'smit';
+
+    public imgUrl: string = 'test.png';
+    public imgAlt: string = 'picture';
+
+    public arr: number[] = [1, 2, 3, 4, 5];
+
+    public user = {
+        name: 'john',
+        surn: 'smit',
+    };
+
+    public user1 = {
+        img: 'pic.png',
+        alt: 'picture',
+    };
+
+    public showName() {
+        return 'john';
+    }
+
+    public showSurn() {
+        return 'smit';
+    }
+
+    public text: string = 'abcde';
+
+    protected readonly title = signal('el-pixel-angular');
+    protected readonly Math = Math;
+    protected readonly asNativeElements = asNativeElements;
 }
