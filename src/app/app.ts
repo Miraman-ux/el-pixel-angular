e4168054031b80bbc20643233b32a7747e87610b
M1raman <276498452+Miraman-ux@users.noreply.github.com>
2026-06-25 12:54:16 +0300

    feat: выполнил 4 этапа

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { asNativeElements, Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { Home } from './home/home';
+import { HousingLocation } from './housing-location/housing-location';
 
 @Component({
     selector: 'app-root',
-    imports: [FormsModule, RouterOutlet, RouterLink],
+    imports: [FormsModule, Home, HousingLocation, RouterOutlet, RouterLink],
     templateUrl: './app.html',
     styleUrls: ['./app.css', './text.css', './border.css'],
 })
